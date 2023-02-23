c2671bc6c3b5aa14ab04f179fcfb41e5a684ed5d
unknown <sxcueto@gmail.com>
2023-02-23 14:11:13 -0500

    push

---
+++ app.js
@@ -0,0 +1,48 @@
+// addition
+var numberOne = 10;
+var numberTwo = 10;
+
+function addTwo(x, y) {
+    console.log(x + y);
+}
+addTwo(numberOne, numberTwo);
+
+const plusTwo = (x, y) => {
+    console.log(x + y);
+}
+
+plusTwo(numberOne, numberTwo)
+
+// subtraction
+function subTwo(x, y) {
+    console.log(x - y);
+}
+subTwo(numberOne, numberTwo);
+
+
+const minusTwo = (x, y) => {
+    console.log(x - y);
+}
+minusTwo(numberOne, numberTwo);
+
+// multiplication
+function mulTwo(x, y) {
+    console.log(x * y);
+}
+mulTwo(numberOne, numberTwo);
+
+const timesTwo = (x,y) => {
+    console.log (x*y);
+}
+timesTwo(numberOne, numberTwo);
+
+// division
+function divTwo(x,y){
+    console.log(x/y);
+}
+divTwo(numberOne, numberTwo);
+
+const calcTwo = (x,y) =>{
+    console.log(x/y);
+}
+calcTwo(numberOne, numberTwo);
